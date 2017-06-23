8425dfa7a5d7945858c618d65f1c40d1699ea89d
hanchi12 <mvcane17@gmail.com>
2017-06-23 10:41:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,27 +36,63 @@ def webhook():
 
 
 def processRequest(req):
-    
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    
-    baseurl = "http://samples.openweathermap.org/data/2.5/weather?zip=01485,ph&appid=66910cc54f55f6bc8de8dc666ccb3120"
-    
-    result = urlopen(baseurl).read()
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res
 
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
 def makeWebhookResult(data):
-    
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
     # print(json.dumps(item, indent=4))
-    
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
     print("Response:")
     print(speech)
 
     return {
-        "speech": "despacito",
-        "displayText": "bespacito",
+        "speech": speech,
+        "displayText": speech,
         # "data": data,
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
